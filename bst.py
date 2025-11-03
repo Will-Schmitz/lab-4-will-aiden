649651dbacae604f0b835f1142f79158f418e817
Will-Schmitz <wpschmit@calpoly.edu>
2025-11-02 23:34:04 -0800

    First Push

---
+++ bst.py
@@ -0,0 +1,22 @@
+import sys 
+import unittest 
+from typing import * 
+from dataclasses import dataclass 
+sys.setrecursionlimit(10**6) 
+
+BinTree: TypeAlias = Union[None, "Node"]
+
+@dataclass(frozen = True)
+class Node:
+    value: Any
+    left: BinTree
+    right: BinTree
+
+@dataclass(frozen = True)
+class BinarySearchTree:
+    comes_before: Callable[[Any, Any], bool]
+    tree: BinTree = None
+
+#Return True iff the BST has no nodes
+def is_empty(bst: BinarySearchTree) -> bool:
+    return bst.tree is None
